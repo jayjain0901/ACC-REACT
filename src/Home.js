287b6a20141a6bdf24db31ce12a06104440d679c
jayjain0901 <57716997+jayjain0901@users.noreply.github.com>
2023-11-08 18:52:30 +0530

    Add files via upload

---
+++ src/Home.js
@@ -0,0 +1,17 @@
+const Home = () => {
+    const handleClick = () => {
+        console.log('hello ninja');
+    }
+    const handleClickAgain = (name) => {
+        console.log('hello ' + name);
+    }
+    return (     
+        <div className="home">
+            <h2>Homepage</h2>
+            <button onClick={handleClick}>Click me !</button>
+            <button onClick={ () => handleClickAgain('Hey') } > Click me again !</button>
+        </div>
+    );
+}
+ 
+export default Home ;
